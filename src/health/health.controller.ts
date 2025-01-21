e9a1d3ae64ac838210467bb661d7c91b0b276a4a
Carlos Cativo <cativo23.kt@gmail.com>
2025-01-20 19:57:04 -0600

    ✨ feat (setup): add health check module and Swagger documentation setup; include Docker configuration and environment variables

---
+++ src/health/health.controller.ts
@@ -0,0 +1,21 @@
+
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck, DiskHealthIndicator } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private readonly disk: DiskHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+    ]);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Set up Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
+  // Set up port  
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  console.log('Listening on port:', port);
+  await app.listen(port);
 }
 bootstrap();
