7e0ed3bdf0b6f69ec6152fe42050bf4e5227b161
Carlos Cativo <cativo23.kt@gmail.com>
2025-05-20 22:07:17 -0600

    ✨ feat(auth): enhance AuthService tests with mock implementations for UsersService, JwtService, and ConfigService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const mockUsersService = {
+      findOneByEmail: jest.fn(),
+      create: jest.fn(),
+    };
+
+    const mockJwtService = {
+      signAsync: jest.fn(),
+    };
+
+    const mockConfigService = {
+      get: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: ConfigService, useValue: mockConfigService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
