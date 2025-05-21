ed8895e679b39872a43630c8a107c55dc421bbba
Carlos Cativo <cativo23.kt@gmail.com>
2025-05-21 01:06:29 -0600

    ✨ feat(projects): add AuthModule to ProjectsModule imports for authentication support

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,9 +3,13 @@ import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
+import { AuthModule } from '@app/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [
+    TypeOrmModule.forFeature([Project]),
+    AuthModule
+  ],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
